fd7ac8d82eef8515f42d820c9c10e51dad54da3f
FranciscoBelando <1514980@alu.murciaeduca.es>
2025-02-19 23:00:42 +0100

    Inicio de repositorio

---
+++ readme.md
@@ -0,0 +1,11 @@
+# 📝 Practica 4.3
+
+Descarga el archivo EjercicioArrays.java y añádelo a un proyecto en Eclipse, examina el código y prueba su funcionamiento. Después, refactoriza el código para reducir al máximo el código sin que afecte a su funcionamiento. Este trabajo debe subirse a una rama específica con el nombre que se desee en un repositorio de GitHub y luego mezclar los cambios en la rama principal y subirlo también al repositorio.
+
+Entrega el archivo "EjercicioArrays.java" refactorizado y un archivo .txt con el enlace al repositorio de GitHub donde se encuentre.
+
+## 📂 Contenido
+Repositorio de [Francisco Belando](https://github.com/FranciscoBelando), que contiene el código fuente de la practica 4.3 de Entornos de Desarrollo.
+
+## 📋
+He modificado por completo el codigo para poder crear un objeto clase donde contenga todos los atributos usados en el codigo original y sus metodos para operar con ellos,la idea original era crear un archivo aparte para hacerlo más modular el codigo,pero me he ceñido al objetivo de la practica de subir un solo archivo.
--- src/ProblemaArray/EjercicioArrays.java
+++ src/ProblemaArray/EjercicioArrays.java
@@ -1,179 +1,156 @@
 package ProblemaArray;
 import java.util.Arrays;
+import java.util.List;
 
 public class EjercicioArrays {
-
-    static class Clase {
-
-        private int numAlumnos;
-        private int[] notas;
-        private int[] listaClase;
-        private Integer[][] MinMax;
-        private int[] practicas;
-        private float[] calFinal;
-        private float[] estadistica;
-        private int[] aproSus;
-
-        public Clase(int numAlumnos) {
-        	
-            this.numAlumnos = numAlumnos;
-            this.notas = new int[numAlumnos];
-            this.listaClase = new int[numAlumnos];
-            this.practicas = new int[numAlumnos];
-            this.calFinal = new float[numAlumnos];
-            this.estadistica = new float[10];
-            this.MinMax = new Integer[2][2];
-            this.aproSus = new int[2];
-
-            // Genera una lista automáticamente al crear el objeto
-            for (int i = 0; i < numAlumnos; i++) {
-                listaClase[i] = i + 1;
-            }
-        }
-
-        public void generarNotas() {
-        	
-            for (int i = 0; i < this.numAlumnos; i++) {
-                this.notas[i] = (int) (Math.random() * 11);
-                this.practicas[i] = (int) (Math.random() * 11);
-            }
-        }
-
-        public float[] MediaCalificaciones() {
-        	
-            for (int i = 0; i < notas.length; i++) {
-                calFinal[i] = (((float) notas[i] + (float) practicas[i]) / 2);
-            }
-            return calFinal;
-        }
-
-        public void calcularEstadistica() {
-        	
-            for (int i = 0; i < 10; i++) {
-                float count = 0;
-                for (int j = 0; j < calFinal.length; j++) {
-                    if ((i < calFinal[j]) && ((i + 1) >= calFinal[j])) {
-                        count += 1;
-                    }
-                }
-                this.estadistica[i] = count / numAlumnos;
-                double sol = (Math.round(this.estadistica[i] * 10000.0)) / 100.0;
-                System.out.println("Estadística nota tramo <=" + (i + 1) + " = " + sol + "%");
-            }
-        }
-
-        public void FindMinMax() {
-        	
-            MinMax = new Integer[][]{{10, 0}, {0, 0}};
-
-            for (int i = 0; i < notas.length; i++) {
-                if (notas[i] < MinMax[0][0]) {
-                    this.MinMax[0][0] = notas[i];
-                    this.MinMax[0][1] = i;
-                }
-                if (notas[i] > MinMax[1][0]) {
-                    this.MinMax[1][0] = notas[i];
-                    this.MinMax[1][1] = i;
-                }
-            }
-        }
-
-        public void AprobadosSuspensos() {
-        	
-            for (int i = 0; i < numAlumnos; i++) {
-                if (notas[i] < 5) {
-                    aproSus[1]++;
-                } else {
-                    aproSus[0]++;
-                }
-            }
-
-            System.out.println("Relación de aprobados por nº de lista: " + aproSus[0]);
-            System.out.println("Relación de suspensos por nº de lista: " + aproSus[1]);
-        }
-
-        public void ResumenAproSus() {
-        	
-            int[][] AproSusList = new int[numAlumnos][numAlumnos];
-
-            for (int i = 0; i < notas.length; i++) {
-                if (notas[i] < 5) {
-                    AproSusList[1][i] = notas[i];
-                } else {
-                    AproSusList[0][i] = notas[i];
-                }
-            }
-            
-            System.out.println("Resumen de aprobados por nº de lista: " + Arrays.toString(AproSusList[1]));
-            System.out.println("Resumen de suspensos por nº de lista: " + Arrays.toString(AproSusList[0]));
-        }
-
-        public void MostrarInfo() {
-        	
-            System.out.println("Mínimo es: " + MinMax[0][0]);
-            System.out.println("Máximo es: " + MinMax[1][0]);
-            System.out.println("Índice del mínimo es: " + MinMax[0][1]);
-            System.out.println("Índice del máximo es: " + MinMax[1][1]);
-            System.out.println("Lista de clase: " + Arrays.toString(listaClase));
-            System.out.println("Array de Notas: " + Arrays.toString(notas));
-            System.out.println("Prácticas: " + Arrays.toString(practicas));
-            System.out.println("Calificaciones: " + Arrays.toString(calFinal));
-        }
-        
-        public void insertarNuevaNota() {
-
-            int[] nuevasNotas = new int[40];
-            
-            for (int j = 0; j < 31; j++) {
-                nuevasNotas[j] = (int) (Math.random() * 11);
-            }
-
-            System.out.println("Nota antigua alumno nº4: " + nuevasNotas[3]);
-            
-            nuevasNotas[3] = 6;
-
-            System.out.println("Nota nueva alumno nº4: " + nuevasNotas[3]);
-
-            this.notas = nuevasNotas;
-        }
-
-        
-    }
-
+    
     public static void main(String[] args) {
-    	
         int numAlumnos = 40;
+        //vector con las notas generadas
+        Integer[] control = new Integer[numAlumnos];
+		int[] listaClase;
+		int[] practicas;
+		float[] calificaciones;
+		float[] estadistica;
+		int[] aprobados;
+		int[] suspensos;
+        int maxNota = 0;
+        int minNota = 0;
+        int indMaxNota, indMinNota;
+        int postEval;
+		double[] calif;
+        //Genera notas random entre 1 y 10
+        for(int i=0; i < control.length; i++){
+            control[i] = (int)(Math.random()*11);
+        }
+        //buscamos al mayor
+        postEval = 11;
+        for(int i=0; i<control.length; i++){
+            int preEval = control[i];
+            if (preEval < postEval){
+                minNota = preEval;
+                postEval = control[i];
+            }
+        }
+        //buscamos al menor
+        postEval = 0;
+        for(int i=0; i<control.length; i++){
+            int preEval = control[i];
+            if (preEval > postEval){
+                maxNota = preEval;
+                postEval = control[i];
+            }
+        }
+        //creamos una lista de los alumnos de la clase
+        listaClase = new int[numAlumnos];
+        for (int i = 0; i < numAlumnos; i++){
+            listaClase[i] = i+1;
+        }
+        //Empezamos el uso de listas para facilitar la tarea de índices.
+        List notas = Arrays.asList(control);
+        indMinNota = notas.indexOf(minNota) + 1;
+        indMaxNota = notas.indexOf(maxNota) + 1;
 
-        // Crea un objeto clase1 y otro llamado clase
-        Clase clase1 = new Clase(numAlumnos);
-
-        // Generar notas aleatorias
-        clase1.generarNotas();
-
-        // Calcular la media de las calificaciones
-        clase1.MediaCalificaciones();
-
-        // Encontrar la nota mínima y máxima junto con su índice
-        clase1.FindMinMax();
-
-        // Mostrar la información general
-        clase1.MostrarInfo();
-
-        // Calcular y mostrar la estadística de notas
-        clase1.calcularEstadistica();
-
-        // Determinar aprobados y suspensos
-        clase1.AprobadosSuspensos();
-
-        // Mostrar resumen de aprobados y suspensos
-        clase1.ResumenAproSus();
+        //Comprobamos el resultado del ejercicio   
+        System.out.println("Mínimo es: " + minNota);
+        System.out.println("Máximo es: " + maxNota);
+        System.out.println("Indice del mínimo es : " + indMinNota);
+        System.out.println("Indice del máximo es : " + indMaxNota);
+        System.out.println("Lista de clase :" + Arrays.toString(listaClase));
+        System.out.println("Array de Notas :" + notas);
         
+        //creamos el array de notas "practicas"
+        practicas = new int[numAlumnos];
+        for(int i=0; i < practicas.length; i++){
+            practicas[i] = (int)(Math.random()*11);
+        }
+        //Creamos el vector calificaciones
+        calificaciones = new float[numAlumnos];
+        for(int i = 0; i<control.length; i++){
+            calificaciones[i] = 
+                    (((float) control[i] 
+                    + (float) practicas[i]) 
+                    / 2);
+        }
+        System.out.println("Prácticas      :" + Arrays.toString(practicas));
+        System.out.println("Calificaciones :" + Arrays.toString(calificaciones));
+        
+        //Sacamos la estadística de calificaciones
+        //hacemos un array de 10 para la estadística.
+        estadistica = new float[10];
+      
+        for (int i=0; i<10; i++){
+            float count = 0;
+            float sum = 0;
+            for (int j=0; j<control.length; j++){
+                if ((i < calificaciones[j]) && ((i+1) >= calificaciones[j] )) {
+                    sum += calificaciones[j];
+                    count += 1;
+                }
+            }
+            if (count != 0){
+                estadistica[i] = ( (float)count / numAlumnos);
+            }else{ estadistica[i] = 0;}
+            double sol = (Math.round(estadistica[i] * 10000.0)) / 100.0;
+            System.out.println("Estadística nota tramo <=" 
+                + (i+1) + " = " 
+                + sol + "%");
+        }
+        //Aprobados y suspensos
+        aprobados = new int[numAlumnos];
+        suspensos = new int[numAlumnos];
+        int countAprobados = 0;
+        int countSuspensos = 0;
+        for (int i=0; i<numAlumnos; i++){
+            if (calificaciones[i] < 5){
+                aprobados[i] = i;
+                countAprobados += 1;
+            }else{ 
+                suspensos[i] = i;
+                countSuspensos += 1;
+            }
+        }        
+        System.out.println("Relación de aprobados por nº de lista: " 
+                + Arrays.toString(aprobados));
+        System.out.println("Relación de suspensos por nº de lista: " 
+                + Arrays.toString(suspensos));
+        //Resumen de aprobados y suspensos
+        int i = 0;
+        int x = 0;
+        int[] a = new int[countAprobados];
+        int[] s = new int[countSuspensos];
+        while(i < aprobados.length){
+            if(aprobados[i] != 0){
+                a[x] = aprobados[i];
+                i++;
+                x++;
+            }else{ i++; }
+        }
+        
+        i = x = 0;
+        while(i < suspensos.length){
+            if(suspensos[i] != 0){
+                s[x] = suspensos[i];
+                i++;
+                x++;
+            }else{ i++; }
+        }
+        System.out.println("Resumen  de aprobados por nº de lista: " 
+                + Arrays.toString(a));
+        System.out.println("Resumen  de aprobados por nº de lista: " 
+                + Arrays.toString(s));
+    
         /*6. Suponer un vector de Calificaciones de tamaño 40 
         (máximo de alumnos por clase), pero que solo almacena las
         notas de 31 alumnos. Realizar un programa que permita insertar en
         la posición 4 del vector la calificación de un nuevo 
         alumno en clase al que supuestamente le corresponde como nota un 6.*/
-        
-        clase1.insertarNuevaNota();
+        calif = new double[40];
+        for (int j=0; j<31; j++){
+            calif[j] = (int)(Math.random()*11);
+        }
+        System.out.println("Nota antigua alumno nº4: " + calif[3]); 
+        calif[3] = 6;
+        System.out.println("Nota nueva   alumno nº4: " + calif[3]);
     }
-} 
+}
